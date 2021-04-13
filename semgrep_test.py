e59a61fe96b57d6337e1580149991fbd0087ce70
Andrew Ratcliffe <andrew.ratcliffe@nswcsystems.co.uk>
2021-04-13 18:51:04 +0100

    removes comments

--- semgrep_test.py
+++ semgrep_test.py
@@ -1,17 +1,12 @@
-# cf. https://github.com/PyCQA/bandit/blob/b78c938c0bd03d201932570f5e054261e10c5750/examples/crypto-md5.py
-
 from cryptography.hazmat.primitives import hashes
 
-# ruleid:insecure-hash-algorithm-md5
+
 hashes.SHA1()
-# ruleid:insecure-hash-algorithm-sha1
+
 hashes.SHA256()
-# ok:insecure-hash-algorithm-sha1
-# ok:insecure-hash-algorithm-md5
-hashes.SHA256()
-# ok:insecure-hash-algorithm-sha1
-# ok:insecure-hash-algorithm-md5
+
 hashes.SHA3_256()
+
 digest = hashes.Hash(hashes.SHA256())
 digest.update(b"abcdefg")
 product = digest.finalize()
