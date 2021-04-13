4ae4c8d799591b1cdd877edcb3e741933c1a466a
Andrew Ratcliffe <andrew.ratcliffe@nswcsystems.co.uk>
2021-04-13 19:15:38 +0100

    update hashes

--- semgrep_test.py
+++ semgrep_test.py
@@ -7,13 +7,13 @@ hashes.SHA256()
 
 hashes.SHA3_256()
 
-digest = hashes.Hash(hashes.MD5())
+digest = hashes.Hash(hashes.SHA1())
 digest.update(b"abcdefg")
 product = digest.finalize()
 print(product.hex())
 # echo -n "abcdefg" | md5
 # 7ac66c0f148de9519b8bd264312c4d64
-fileDigest = hashes.Hash(hashes.SHA256())
+fileDigest = hashes.Hash(hashes.SHA1())
 
 with open("test.txt", 'rb') as reader:
     # Read and add line to fileDigest
