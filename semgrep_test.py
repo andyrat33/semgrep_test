06e802229830a0fd6de0aa97f0b2ffe02468355a
Andrew Ratcliffe <andrew.ratcliffe@nswcsystems.co.uk>
2021-04-13 22:01:53 +0100

    corrects all crypto weaknesses

--- semgrep_test.py
+++ semgrep_test.py
@@ -3,13 +3,13 @@ from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives import hashes
 
 
-digest = hashes.Hash(hashes.SHA1())
+digest = hashes.Hash(hashes.SHA256())
 digest.update(b"abcdefg")
 product = digest.finalize()
 print(product.hex())
 # echo -n "abcdefg" | md5
 # 7ac66c0f148de9519b8bd264312c4d64
-fileDigest = hashes.Hash(hashes.MD5())
+fileDigest = hashes.Hash(hashes.SHA256())
 
 with open("test.txt", 'rb') as reader:
     # Read and add line to fileDigest
@@ -21,10 +21,11 @@ print(fileProduct.hex())
 # 298b5f666156316f802ea35fb2ee887d
 # md5 test.txt
 # MD5 (test.txt) = 298b5f666156316f802ea35fb2ee887d
-ec.generate_private_key(curve=ec.SECP192R1, backend=backends.default_backend())
-
+ec.generate_private_key(curve=ec.SECP256K1, backend=backends.default_backend())
+# SECP256K1 Good
+# SECP192R1 & SECT163K1 Bad
 # ruleid: insufficient-ec-key-size
-ec.generate_private_key(ec.SECT163K1, backends.default_backend())
+ec.generate_private_key(ec.SECP256K1, backends.default_backend())
 
 print("Test hash algorithm MD5 is weak")
 print("Done")
