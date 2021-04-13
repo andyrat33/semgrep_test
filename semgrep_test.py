5c4f85f3998963c77e28e6c17836b85d1e7db490
Andrew Ratcliffe <andrew.ratcliffe@nswcsystems.co.uk>
2021-04-13 14:39:51 +0100

    adds file test.txt and calc md5 hash of file

--- semgrep_test.py
+++ semgrep_test.py
@@ -18,3 +18,15 @@ product = digest.finalize()
 print(product.hex())
 # echo -n "abcdefg" | md5
 # 7ac66c0f148de9519b8bd264312c4d64
+fileDigest = hashes.Hash(hashes.MD5())
+
+with open("test.txt", 'rb') as reader:
+    # Read and add line to fileDigest
+    for line in reader:
+        fileDigest.update(line)
+
+fileProduct = fileDigest.finalize()
+print(fileProduct.hex())
+# 298b5f666156316f802ea35fb2ee887d
+# md5 test.txt
+# MD5 (test.txt) = 298b5f666156316f802ea35fb2ee887d
